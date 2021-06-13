b143236c30a05ae67b541b40d43c1d3fff275ad8
vikito755 <35623614+vikito755@users.noreply.github.com>
2021-06-13 19:25:57 +0000

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,32 +1,13 @@
-//...
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => {
-            if (response) {
-                //found cached resource
-                return response;
-            }
+const CACHE_NAME = 'site-name-cache';
 
-            // get resource and add it to cache
-            return fetch(event.request)
-                .then(response => {
-                    // check if the response is valid
-                    if (!response.ok) {
-                        return response;
-                    }
-
-                    // clone the response
-                    const newResponse = response.clone();
-
-                    // add it to cache
-                    caches.open(CACHE_NAME)
-                        .then(cache =>
-                            cache.put(event.request, newResponse)
-                        );
-
-                    // return response
-                    return response;
-                });
-        })
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches
+            .open(CACHE_NAME)
+            .then(cache =>
+                cache.addAll([
+                    window.location.href
+                ])
+            )
     );
 });
